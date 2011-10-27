0d47fbfe3cd52d999851a796c23c1b98dcca7970
Alex Nichol <alexnichol@comcast.net>
2011-10-27 19:46:26 -0400

    Initial Commit

---
+++ AsyncCopy/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  AsyncCopy
+//
+//  Created by Alex Nichol on 10/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
